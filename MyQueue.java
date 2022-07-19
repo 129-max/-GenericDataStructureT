2adedac54ebce45aea73130cfc2e7ceea8806930
Mohit Raj <rajmohit.129@gmail.com>
2022-07-20 03:10:52 +0530

    [Mohit]Added:to create a queue

---
+++ MyQueue.java
@@ -0,0 +1,21 @@
+public class MyQueue {
+
+
+    private final CreatedLinkedList linkList;
+
+    public MyQueue() {
+        this.linkList = new CreatedLinkedList();
+    }
+
+    public void enqueue(MyNode newNode) {
+        linkList.addAtStart(newNode);
+    }
+
+    public MyNode peek() {
+        return linkList.last;
+    }
+
+    public MyNode dequeue() {
+        return linkList.RemoveAtLast();
+    }
+}
